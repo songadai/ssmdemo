7d13054395d9efad42f2c127d58a55b93f64cad0
songadai <1456073180@qq.com>
2019-06-05 13:00:57 +0800

    "提交测试1"

--- src/main/java/mtt/ls/util/Test2.java
+++ src/main/java/mtt/ls/util/Test2.java
@@ -2,14 +2,11 @@
  * 版权所有 (C) 2019-2029 长沙新时科技发展有限公司。保留所有权利。
  * 版本：
  * 修改记录：
- * 1、2019年06月04天，劉松创建。
+ * 1、2019年06月05天，劉松创建。
  */
 package mtt.ls.util;
 
-public class Test
+public class Test2
 {
-    //public sdfl;
-    public void test(){
-        sdfsdf
-    }
+    提交测试1
 }
